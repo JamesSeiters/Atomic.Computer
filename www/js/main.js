c421abee96faaeaea404fc975001882a8bcce626
James Seiters <jseiters@atomic.computer>
2018-12-31 15:04:50 -0500

    Fixed font issue in SVG logo. Cleaned up HTML.

--- www/js/main.js
+++ www/js/main.js
@@ -2,10 +2,12 @@ $(function(){
 
     var $header = $('#header');
     var $footer = $('#footer');
-    var $logo = $('#logo');
+    var $logo = $('.logo-image-container');
+    var $logotext = $('.logo-text-container');
     var $window = $(window).on('resize', function(){
        var height = $(this).height() - ($header.height() + $footer.height());
        $logo.height(height);
+       $logotext.height(height);
     }).trigger('resize'); //on page load
 
 });
